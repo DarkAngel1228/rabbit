31080d724a45574e5528f3bc6c006fcc3169e95e
zhangjinyu <363626256@qq.com>
2021-11-09 22:55:14 +0800

    玩

---
+++ rabbit-producer/src/main/java/com/rabbit/rabbitproducer/RabbitSender.java
@@ -0,0 +1,56 @@
+package com.rabbit.rabbitproducer;
+
+import org.springframework.amqp.AmqpException;
+import org.springframework.amqp.core.MessagePostProcessor;
+import org.springframework.amqp.rabbit.connection.CorrelationData;
+import org.springframework.amqp.rabbit.core.RabbitTemplate;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.messaging.Message;
+import org.springframework.messaging.MessageHeaders;
+import org.springframework.messaging.support.MessageBuilder;
+import org.springframework.stereotype.Component;
+
+import java.util.Map;
+import java.util.UUID;
+
+@Component
+public class RabbitSender {
+
+    @Autowired
+    private RabbitTemplate rabbitTemplate;
+
+
+    /**
+     * 这里就是确认消息的回调监听接口，用于确认消息是否被broker所收到
+     */
+    final RabbitTemplate.ConfirmCallback confirmCallback = new RabbitTemplate.ConfirmCallback() {
+
+        /**
+         *
+         * @param correlationData 作为一个唯一标识
+         * @param ack 是否落盘成功
+         * @param cause 失败的一些异常信息
+         */
+        @Override
+        public void confirm(CorrelationData correlationData, boolean ack, String cause) {
+            System.err.println("消息ACK结果：" + ack + "，correlationData:" + correlationData.getId());
+        }
+    };
+
+
+    public void send(Object message, Map<String, Object> properties) throws Exception {
+        MessageHeaders mhs = new MessageHeaders(properties);
+        Message<Object> msg = MessageBuilder.createMessage(message, mhs);
+        // 指定业务唯一的id
+        CorrelationData correlationData = new CorrelationData(UUID.randomUUID().toString());
+
+        rabbitTemplate.setConfirmCallback(confirmCallback);
+        rabbitTemplate.convertAndSend("exchange-1", "springboot.rabbit", msg, new MessagePostProcessor() {
+            @Override
+            public org.springframework.amqp.core.Message postProcessMessage(org.springframework.amqp.core.Message message) throws AmqpException {
+                return null;
+            }
+        }, correlationData);
+    }
+
+}
