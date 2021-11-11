c5fc3edb6f7174354be7f2a9bfff5e0c2ced868f
zhangjinyu <363626256@qq.com>
2021-11-11 21:26:46 +0800

    1

--- rabbit-parent/rabbit-core-producer/src/main/java/com/imooc/broker/RabbitBrokerImpl.java
+++ rabbit-parent/rabbit-core-producer/src/main/java/com/imooc/broker/RabbitBrokerImpl.java
@@ -54,26 +54,26 @@ public class RabbitBrokerImpl implements RabbitBroker {
 
     @Override
     public void reliantSend(Message message) {
-        message.setMessageType(MessageType.RELIANT);
-        BrokerMessage bm = messageStoreService.selectByMessageId(message.getMessageId());
-
-        if (bm == null) {
-            // 1.把数据库的消息发送日志先记录好
-            Date now = new Date();
-            BrokerMessage brokerMessage = new BrokerMessage();
-            brokerMessage.setMessageId(message.getMessageId());
-            brokerMessage.setStatus(BrokerMessageStatus.SENDING.getCode());
-
-            // tryCount 在最开始发送的时候不需要进行设置
-            brokerMessage.setNewRetry(DateUtils.addMinutes(now, BrokerMessageConst.TIMEOUT));
-            brokerMessage.setCreateTime(now);
-            brokerMessage.setUpdateTime(now);
-            brokerMessage.setMessage(message);
-            messageStoreService.insert(brokerMessage);
-        }
-
-        // 2.执行真正的发送消息逻辑
-        sendKernel(message);
+//        message.setMessageType(MessageType.RELIANT);
+//        BrokerMessage bm = messageStoreService.selectByMessageId(message.getMessageId());
+//
+//        if (bm == null) {
+//            // 1.把数据库的消息发送日志先记录好
+//            Date now = new Date();
+//            BrokerMessage brokerMessage = new BrokerMessage();
+//            brokerMessage.setMessageId(message.getMessageId());
+//            brokerMessage.setStatus(BrokerMessageStatus.SENDING.getCode());
+//
+//            // tryCount 在最开始发送的时候不需要进行设置
+//            brokerMessage.setNewRetry(DateUtils.addMinutes(now, BrokerMessageConst.TIMEOUT));
+//            brokerMessage.setCreateTime(now);
+//            brokerMessage.setUpdateTime(now);
+//            brokerMessage.setMessage(message);
+//            messageStoreService.insert(brokerMessage);
+//        }
+//
+//        // 2.执行真正的发送消息逻辑
+//        sendKernel(message);
 
     }
 
@@ -84,19 +84,19 @@ public class RabbitBrokerImpl implements RabbitBroker {
 
 
     private void sendKernel(Message message) {
-        AsyncBaseQueue.submit((Runnable) () -> {
-            CorrelationData correlationData =
-                    new CorrelationData(String.format("%s#%s#%s",
-                            message.getMessageId(),
-                            System.currentTimeMillis(),
-                            message.getMessageType()));
-            String topic = message.getTopic();
-            String routingKey = message.getRoutingKey();
-            RabbitTemplate rabbitTemplate = rabbitTemplateContainer.getTemplate(message);
-            rabbitTemplate.convertAndSend(topic, routingKey, message, correlationData);
-            log.info("#RabbitBrokerImpl.sendKernel# send to rabbitmq, messageId: {}", message.getMessageId());
-
-
-        });
+//        AsyncBaseQueue.submit((Runnable) () -> {
+//            CorrelationData correlationData =
+//                    new CorrelationData(String.format("%s#%s#%s",
+//                            message.getMessageId(),
+//                            System.currentTimeMillis(),
+//                            message.getMessageType()));
+//            String topic = message.getTopic();
+//            String routingKey = message.getRoutingKey();
+//            RabbitTemplate rabbitTemplate = rabbitTemplateContainer.getTemplate(message);
+//            rabbitTemplate.convertAndSend(topic, routingKey, message, correlationData);
+//            log.info("#RabbitBrokerImpl.sendKernel# send to rabbitmq, messageId: {}", message.getMessageId());
+//
+//
+//        });
     }
 }
